0135532341c74a65cd2e2d32cc6e34232f24919d
Santiago Sandoval <casaba3131@gmail.com>
2023-10-04 22:22:50 -0500

    Se refactoriza el codigo

--- src/app/modules/detail-portafolio/detail-portafolio.component.ts
+++ src/app/modules/detail-portafolio/detail-portafolio.component.ts
@@ -11,6 +11,23 @@ import { PortafolioService } from 'src/app/services/portafolio.service';
 export class DetailPortafolioComponent implements OnInit {
 
   facility: Facility = new Facility();
+  facilityNotFound: Facility = {
+    locationNumber: 'Numero de local no encontrado',
+    name: 'Nombre de local no encontrado',
+    description: 'Descripcion de local no encontrado',
+    businessType: 'Tipo de negocio de local no encontrado',
+    locatedFloor: 'Piso de local no encontrado',
+    openingHours: 'Horario de local no encontrado',
+    phoneNumber: '00000000',
+    webPage: 'https://www.google.com/',
+    image1: 'Imagen de local no encontrado',
+    image2: 'Imagen de local no encontrado',
+    image3: 'Imagen de local no encontrado',
+    logo: 'Logo de local no encontrado',
+    whasApp: 'https://web.whatsapp.com/',
+    facebook: 'https://www.facebook.com/',
+    instagram: 'https://www.instagram.com/'
+  }
 
   constructor(private portafolioService: PortafolioService, private activatedRoute: ActivatedRoute) { }
 
@@ -22,10 +39,10 @@ export class DetailPortafolioComponent implements OnInit {
   getFacility(id:string){
     this.portafolioService.getFacility(id).subscribe({
       next: (res) => {
-        this.facility = res
+        this.facility = res;
       },
       error: (err) => {
-        // redirigir a 404
+        this.facility = this.facilityNotFound;
       }
     })
   }
--- src/app/services/portafolio.service.ts
+++ src/app/services/portafolio.service.ts
@@ -13,13 +13,13 @@ export class PortafolioService {
 
     { locationNumber: 'local 1001', name: 'Frisby', description: 'Restaurante con mas de 20 años de experiencia', businessType: 'restaurante', locatedFloor: '1', openingHours: '11:00 - 23:00', phoneNumber: '123456789', webPage: 'https://frisby.com.co/', image1: 'https://upload.wikimedia.org/wikipedia/commons/1/10/Tienda_Frisby_Suba%2C_Bogot%C3%A1.jpg', image2: 'https://www.institucionalcolombia.com/wp-content/uploads/2019/09/Caso-Frisby.jpg', image3: 'https://10619-2.s.cdn12.com/rests/original/110_506383782.jpg', logo: 'https://primaveraurbana.co/wp-content/uploads/2022/06/frisby_-1.jpg', whasApp: '345345345', facebook: 'https://www.facebook.com/', instagram: 'https://www.instagram.com/' },
 
-    { locationNumber: 'local 1002', name: 'Papel a todo', description: 'Restaurante con mas de 20 años de experiencia', businessType: 'papeleria', locatedFloor: '1', openingHours: '11:00 - 23:00', phoneNumber: '123456789', webPage: 'www.frisby.com', image1: 'assets/images/frisby1.jpg', image2: 'assets/images/frisby2.jpg', image3: 'assets/images/frisby3.jpg', logo: 'assets/images/frisby-logo.jpg', whasApp: 'https://web.whatsapp.com/', facebook: 'https://www.facebook.com/', instagram: 'https://www.instagram.com/' },
+    { locationNumber: 'local 1002', name: 'Papel a todo', description: 'Restaurante con mas de 20 años de experiencia', businessType: 'papeleria', locatedFloor: '1', openingHours: '11:00 - 23:00', phoneNumber: '123456789', webPage: 'www.frisby.com', image1: 'assets/img/portfolio/1.jpg', image2: 'assets/images/frisby2.jpg', image3: 'assets/images/frisby3.jpg', logo: 'assets/images/frisby-logo.jpg', whasApp: 'https://web.whatsapp.com/', facebook: 'https://www.facebook.com/', instagram: 'https://www.instagram.com/' },
 
-    { locationNumber: 'local 1003', name: 'Papeleria', description: 'Restaurante con mas de 20 años de experiencia', businessType: 'papeleria', locatedFloor: '1', openingHours: '11:00 - 23:00', phoneNumber: '123456789', webPage: 'www.frisby.com', image1: 'assets/images/frisby1.jpg', image2: 'assets/images/frisby2.jpg', image3: 'assets/images/frisby3.jpg', logo: 'assets/images/frisby-logo.jpg', whasApp: 'https://web.whatsapp.com/', facebook: 'https://www.facebook.com/', instagram: 'https://www.instagram.com/' },
+    { locationNumber: 'local 1003', name: 'Papeleria', description: 'Restaurante con mas de 20 años de experiencia', businessType: 'papeleria', locatedFloor: '1', openingHours: '11:00 - 23:00', phoneNumber: '123456789', webPage: 'www.frisby.com', image1: 'assets/img/portfolio/1.jpg', image2: 'assets/images/frisby2.jpg', image3: 'assets/images/frisby3.jpg', logo: 'assets/images/frisby-logo.jpg', whasApp: 'https://web.whatsapp.com/', facebook: 'https://www.facebook.com/', instagram: 'https://www.instagram.com/' },
   ];
 
   categories: Array<Categories> = [
-    {id: '1', description: 'restaurante', image: 'https://www.visitfinland.com/dam/jcr:7c2c8221-ace3-4362-a5b0-8073ee6967e9/Savoy-Interior-7.jpeg'},
+    {id: '1', description: 'restaurante', image: 'https://www.eltiempo.com/files/image_640_428/uploads/2022/11/11/636ec9b036dfd.png'},
     {id: '2', description: 'papeleria', image: 'assets/img/portfolio/1.jpg'},
   ];
 
